fe54fa50c98bf33c97537396f983551179cbee5f
richchen <809155736@qq.com>
2020-07-20 19:57:16 +0800

    added some dep

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Cookies from 'js-cookie'
+import Element from 'element-ui'
+import './assets/styles/element-variables.scss'
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
+  outputDir: 'dist',
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+  productionSourceMap: false
+}
