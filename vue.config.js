15d34a7b103b9ac58c1ab84d12a05d6175ec1d09
richchen <809155736@qq.com>
2020-07-23 18:41:09 +0800

    消除proxy问题

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,28 @@ const path = require('path')
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
+const port = process.env.port || process.env.npm_config_port || 80 // 端口
 module.exports = {
   // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
   outputDir: 'dist',
   // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   productionSourceMap: false,
+  devServer: {
+    host: '0.0.0.0',
+    port: port,
+    proxy: `http://localhost:8080`,
+    // proxy: {
+    //   // detail: https://cli.vuejs.org/config/#devserver-proxy
+    //   [process.env.VUE_APP_BASE_API]: {
+    //     target: `http://localhost:8080`,
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       ['^' + process.env.VUE_APP_BASE_API]: ''
+    //     }
+    //   }
+    // },
+    disableHostCheck: true
+  },
   chainWebpack(config) {
     // set svg-sprite-loader
     config.module
