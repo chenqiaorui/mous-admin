a7b01a52e54102fa4f768ca9e3ec4a7f56e53eb7
richchen <809155736@qq.com>
2020-07-23 14:05:58 +0800

    added css styles

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
 import Cookies from 'js-cookie'
 import Element from 'element-ui'
 import './assets/styles/element-variables.scss'
+
+import '@/assets/styles/index.scss' // global css
+import '@/assets/styles/ruoyi.scss' // ruoyi css
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
